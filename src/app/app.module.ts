76d92b2bd34385d73c794a06256f17b3693cdd39
Alexis <paulpulamarin141220@gmail.com>
2024-11-08 16:21:38 -0500

    Clase 8112021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {environment} from '../environments/environment'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {getAuth,provideAuth} from '@angular/fire/auth'
+import {getStorage, provideStorage} from '@angular/fire/storage'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+  provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJn_tGbz44coLI1vINPmbJMCOkaEBts5s",
+    authDomain: "balexis-8a07a.firebaseapp.com",
+    projectId: "balexis-8a07a",
+    storageBucket: "balexis-8a07a.firebasestorage.app",
+    messagingSenderId: "103493038467",
+    appId: "1:103493038467:web:46ec24b9a4c454083dc6b6",
+    measurementId: "G-GCW29L1C8H"
+  }
 };
 
 /*
